bb00d253d88212fe78ba4de0177fc2b941f61ece
Gal Barak <gal.barak2510@gmail.com>
2022-04-26 12:26:35 +0300

    first upload

---
+++ root-cmp.jsx
@@ -0,0 +1,12 @@
+
+const Router = ReactRouterDOM.HashRouter
+const { Route, Switch } = ReactRouterDOM
+
+export function App() {
+    return <Router>
+        <section className="app">
+            hello from app
+        </section>
+    </Router>
+}
+
